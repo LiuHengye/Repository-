d09838bdfcb516f227f019197c12b5a6d66a28b7
LiuHengye <30278082+LiuHengye@users.noreply.github.com>
2018-05-02 21:01:53 +0800

    等等
    等等

---
+++ 视频开发/视频开发/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视频开发
+//
+//  Created by LiuHengye on 2018/5/2.
+//  Copyright © 2018年 Hye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 视频开发/视频开发/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  视频开发
+//
+//  Created by LiuHengye on 2018/5/2.
+//  Copyright © 2018年 Hye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
